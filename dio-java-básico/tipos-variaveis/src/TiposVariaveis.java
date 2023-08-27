bb5c1139b092265b6e5a6f23434a2a94166f7909
DanielMensor <danielmmsmensor@gmail.com>
2023-08-27 19:00:35 -0300

    constantes

--- dio-java-básico/tipos-variaveis/src/TiposVariaveis.java
+++ dio-java-básico/tipos-variaveis/src/TiposVariaveis.java
@@ -1,11 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-       int numeroInteiro = 2;
+       // Variaveis
+        int numeroInteiro = 2;
        double numeorComVirgula = 5.4;
        String palavras = "Hello World";
        boolean verdadeiroFalso = true;
        float numeroComVirgulaMenor = 5.3f;
        long inteiroMaior = 88888888888888L;
     
+
+       // Constantes
+
+       final double VALOR_DE_PI = 3.14;
+       //não pode ser alterada variavel por convençao escrita em caixa alta
     }
 }
