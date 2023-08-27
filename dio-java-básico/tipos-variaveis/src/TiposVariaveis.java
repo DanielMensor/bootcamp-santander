8560d93b18d6b3e33750d38fda3d938aba5581dc
DanielMensor <danielmmsmensor@gmail.com>
2023-08-27 18:57:36 -0300

    algumas variáveis Tipos de Variávies

---
+++ dio-java-básico/tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,11 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+       int numeroInteiro = 2;
+       double numeorComVirgula = 5.4;
+       String palavras = "Hello World";
+       boolean verdadeiroFalso = true;
+       float numeroComVirgulaMenor = 5.3f;
+       long inteiroMaior = 88888888888888L;
+    
+    }
+}
