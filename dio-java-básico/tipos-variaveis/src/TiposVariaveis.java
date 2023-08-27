a6eb014352ef109329876033aac68141dec3a87a
DanielMensor <danielmmsmensor@gmail.com>
2023-08-27 19:02:00 -0300

    arrumado o código de tipos de variaveis

--- dio-java-básico/tipos-variaveis/src/TiposVariaveis.java
+++ dio-java-básico/tipos-variaveis/src/TiposVariaveis.java
@@ -3,15 +3,18 @@ public class TiposVariaveis {
        // Variaveis
         int numeroInteiro = 2;
        double numeorComVirgula = 5.4;
-       String palavras = "Hello World";
        boolean verdadeiroFalso = true;
        float numeroComVirgulaMenor = 5.3f;
        long inteiroMaior = 88888888888888L;
-    
-
+       
+       
        // Constantes
-
+       
        final double VALOR_DE_PI = 3.14;
        //não pode ser alterada variavel por convençao escrita em caixa alta
+       
+       
+       // classe String
+       String palavras = "Hello World";
     }
 }
