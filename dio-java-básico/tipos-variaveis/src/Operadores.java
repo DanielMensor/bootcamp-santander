dd49570d5888e927ca477325b5822befde9ad79e
DanielMensor <danielmmsmensor@gmail.com>
2023-09-10 16:53:05 -0300

    Adicionado Operadores

---
+++ dio-java-básico/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,59 @@
+public class Operadores {
+    
+
+    public static void main(String[] args) {
+        double soma = 10.5 + 15.7; // +  para soma
+
+        int subtracao = 113 - 25; // - para subtraçãp
+
+        int multiplicacao = 20 * 7 ; // * para multiplicação
+
+        int divisao = 15/3 ; // / para divisão
+
+        int modulo = 19 % 3; // % para modulo, pega o resto da divisão
+
+        double parenteses = (10 * 7) + (20/4); // parenteses para separar e fazer um ix de operadores;
+
+        String nomeCompleto = "Daniel" + "Mensor"; // com strings ele concatena tornando o nomeCompleto "DanielMensor"
+
+
+        //Operadores unários
+         /*
+          * + para positivo
+            - para negativo
+            ++ para incremento
+            -- para decremento
+            ! para negação
+          */
+
+        //Ternário ?
+        int a , b;
+        a= 5;
+        b = 5;
+        String resultado = a==b ? "verdadeiro" : "falso";
+        System.out.println(resultado); 
+        /*
+         * <Expressão condicional> ? <Cado condição seja true> : <Caso condição seja false>
+         */
+
+         // Relacionais
+
+        /*
+          * 
+            == igual á
+            != diferente de
+            > maior
+            >= maior ou igual
+            < menor
+            <= menor ou igual
+        */
+
+        // Lógicos
+
+        /*
+         * && "E"
+         * || "OU"
+         */
+
+    }
+}
